a56583b88e182cbea201be3f114ae5fb752b71d6
pin <evinda241@gmail.com>
2026-05-30 19:30:07 +0700

    clean the code

--- script.js
+++ script.js
@@ -6,12 +6,13 @@ function getComputerChoice() {
 function playGame() {
   let humanScore = 0;
   let computerScore = 0;
+  
   const humanScoreText = document.querySelector(".score-box:nth-child(1) p");
   const computerScoreText = document.querySelector(".score-box:nth-child(2) p");
 
   function playRound(humanChoice, computerChoice) {
     if (humanScore === 5 || computerScore === 5) return;
-    const resultText = document.querySelector(".result-text h2");
+    
 
     if (humanChoice === computerChoice) {
       console.log(`Draw! Both chose ${humanChoice}`);
@@ -22,11 +23,13 @@ function playGame() {
     if ((humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissors" && computerChoice === "paper")) {
       humanScore++;
       humanScoreText.textContent = humanScore;
+
       console.log(`You win! ${humanChoice} beats ${computerChoice}`);
       resultText.textContent = `You win! ${humanChoice} beats ${computerChoice}!`;
     } else {
       computerScore++;
       computerScoreText.textContent = computerScore;
+
       console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
       resultText.textContent = `You lose! ${computerChoice} beats ${humanChoice}!`;
     }
@@ -35,9 +38,11 @@ function playGame() {
 
     if (humanScore === 5) {
       console.log("You win the game!");
+
       resultText.textContent = "Congratulations! You win the game!";
     } else if (computerScore === 5) {
       console.log("Computer wins the game!");
+      
       resultText.textContent = "Computer wins the game! Better luck next time!";
     }
   }
